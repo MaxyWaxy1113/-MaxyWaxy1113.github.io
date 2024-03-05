eed8c2516b6256511083e961ce70cc9f64510fe3
MaxyWaxy1113 <liumax1113@gmail.com>
2024-03-05 13:23:00 -0600

    random change

--- index.md
+++ index.md
@@ -5,4 +5,5 @@
 
 ## Projects
 - [interactive scene](scene)
+
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
--- scene/sketch.js
+++ scene/sketch.js
@@ -1,112 +1,39 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+rainbow = prompt("what is your favorite color of the rainbow");
+// figure out in class prompt ("Nice Choice, now check this out")
 
-
-Cookies
-The p5.js Editor uses cookies. Some are essential to the website functionality and allow you to manage an account and preferences. Others are not essential—they are used for analytics and allow us to learn more about our community. We never sell this data or use it for advertising. You can decide which cookies you would like to allow, and learn more in our Privacy Policy.
-
-
-Allow All
-
-Allow Essential
-File
-Edit
-Sketch
-Help
-English
-Log in
-or
-Sign up
-
-
-
-Auto-refresh
-
-Gravity simulation
-by NC_Productions
-
-
-
-sketch.js
-1
-let planets,
-2
-    translation;
-3
-​
-4
 function setup() {
-5
   createCanvas(800, 800);
-6
-  planets = [
-7
-    new Planet(10000, 400, 400, 0, 0, "#EC5"),
-8
-    new Planet(15, 200, 400, 0, -16, "#5CE"),
-9
-    new Planet(50, 400, 700, 15, 0, "#5EA"),
-10
-    new Planet(100, 400, 0, 13, 0, "#CCC")
-11
-  ]
-12
-  translation = createVector(0, 0);
-13
-  //frameRate(20);
-14
+  colorMode(HSB, 360, 100, 100, 100);
 }
-15
-​
-16
+
 function draw() {
-17
-  translate(translation);
-18
-  background(22);
-19
-  for (let a = 0; a < planets.length; a++) {
-20
-    for (let b = 0; b < planets.length; b++) {
-21
-      planets[a].calculateForce(planets[b]);
-22
-    }
-23
+  background(220);
+
+  drawGradient();
+
+  greenGradient();
+
+  drawingContext.fillStyle = gradient;
+  rect(width / 2, height / 2, 400, 400, 50);
+}
+
+function greenGradient() {
+  gradient.addColorStop(0, color(255, 192, 203));
+  gradient.addColorStop(1, color(150, 100, 100));
+}
+
+function favColor() {
+  if (rainbow === "green") {
+    greenGradient();
   }
-24
-  for (const planet of planets) {
-25
-    planet.applyForce();
-26
-    planet.show();
-27
-  }
-28
-  // planet[0].speed = createVector(0, 0);
-29
-  if (keyIsPressed) {
-30
-    switch (key) {
-31
-      case "ArrowLeft":
-32
-        translation.x += 1;
-33
-        break;
-34
-      case "ArrowRight":
-35
-        translation.x -= 1;
-There are no lint messages
-Current line
+}
 
-Console
-Clear
+function drawGradient() {
+  gradient = drawingContext.createLinearGradient(
+    width / 2 - 200,
+    height / 2 - 200,
+    width / 2 + 200,
 
-Preview
-
+    height / 2 + 200
+  );
+}
