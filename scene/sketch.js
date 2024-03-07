579ad098b09ccbffd5876d3be7337d21377acf77
MaxyWaxy1113 <liumax1113@gmail.com>
2024-03-07 13:52:36 -0600

    Added state variable

--- scene/sketch.js
+++ scene/sketch.js
@@ -1,5 +1,7 @@
-rainbow = prompt("what is your favorite color of the rainbow");
-// figure out in class prompt ("Nice Choice, now check this out")
+
+
+
+state = "start screen";
 
 function setup() {
   createCanvas(800, 800);
@@ -11,12 +13,22 @@ function draw() {
 
   drawGradient();
 
-  greenGradient();
-  redGradient();
-  yellowGradient();
+  startScreen();
 
   drawingContext.fillStyle = gradient;
   rect(width / 2, height / 2, 400, 400, 50);
+}
+
+startScreen();
+
+function startScreen() {
+  if (state === "start screen") {
+    background(0)
+    fill("blue");
+    textSize(42);
+    textAlign(CENTER, CENTER);
+    text("Click the mouse to start!", width / 2, height / 2);
+  }
 }
 
 function greenGradient() {
@@ -40,6 +52,34 @@ function yellowGradient() {
   }
 }
 
+function blueGradient() {
+  if (rainbow === "blue") {
+    gradient.addColorStop(0, color(0, 0, 255));
+    gradient.addColorStop(1, color(115, 147, 179));
+  }
+}
+
+function indigoGradient() {
+  if (rainbow === "indigo") {
+    gradient.addColorStop(0, color(75, 0, 130));
+    gradient.addColorStop(1, color(200, 100, 50));
+  }
+}
+
+function violetGradient() {
+  if (rainbow === "violet") {
+    gradient.addColorStop(0, color(127, 0, 255));
+    gradient.addColorStop(1, color(255, 0, 0));
+  }
+}
+
+function orangeGradient() {
+  if (rainbow === "orange") {
+    gradient.addColorStop(0, color(255, 165, 0));
+    gradient.addColorStop(1, color(255, 215, 0));
+  }
+}
+
 function drawGradient() {
   gradient = drawingContext.createLinearGradient(
     width / 2 - 200,
@@ -50,8 +90,13 @@ function drawGradient() {
   );
 }
 
-
-
-
-
-
+function rainbowGradient() {
+  rainbow = prompt("what is your favorite color of the rainbow");
+  greenGradient();
+  redGradient();
+  yellowGradient();
+  blueGradient();
+  indigoGradient();
+  violetGradient();
+  orangeGradient();
+}
