666d95d3883c7f6623dbd4a0b6a3e59cc05dd3a0
MaxyWaxy1113 <liumax1113@gmail.com>
2024-03-22 14:11:32 -0600

    updated arrays and object notation project

--- arrays and object notation scene/sketch.js
+++ arrays and object notation scene/sketch.js
@@ -5,11 +5,24 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let x;
+let dx = 10;
+let dx2 = -10;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  x = width/2
 }
 
 function draw() {
   background(220);
+  fill (100);
+  rect(x, height - 10, 10, 10);
+  Movement();
+}
+
+function Movement() {
+  if (keyIsDown(65)){
+    x -= dx;
+  }
 }
